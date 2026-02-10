b66acf21f4ad9251c7bd487c804b6d4fe19c6377
Christian Stout <christian.allen.stout@gmail.com>
2026-02-09 22:55:26 -0500

    added better editor options, support for odin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,6 +4,28 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("v", "J", ":m '>+1<CR>gv=gv") -- move on select
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "n", "nzzzv") -- Keep searches in the center of the screen
+map("n", "N", "Nzzzv")
+
+map("x", "<leader>p", "\"_dP")
+
+map("n", "d", "\"_d")
+map("v", "d", "\"_d")
+map("n", "x", "\"_x")
+map("v", "x", "\"_x")
+
+map("n", "<leader>d", "\"+d")
+map("v", "<leader>d", "\"+d")
+map("n", "<leader>x", "\"+x")
+map("v", "<leader>x", "\"+x")
+
+map("i", "<C-c>", "<ESC>")
+
+map("n", "Q", "<nop>")
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,63 +13,72 @@ return {
     end,
   },
 
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   config = function()
-  --     local cmp = require("cmp")
-  --
-  --     cmp.setup({
-  --       mapping = {
-  --         -- ONLY accept completion with Ctrl+y
-  --         ["<C-y>"] = cmp.mapping.confirm({
-  --           behavior = cmp.ConfirmBehavior.Insert,
-  --           select = false,
-  --         }),
-  --
-  --         -- Enter does NOTHING related to cmp
-  --         ["<CR>"] = cmp.mapping(function(fallback)
-  --           fallback()
-  --         end),
-  --
-  --         ["<C-e>"] = cmp.mapping.abort(),
-  --         ["<C-n>"] = cmp.mapping.select_next_item(),
-  --         ["<C-p>"] = cmp.mapping.select_prev_item(),
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        side = "right",
+      },
+    }
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function(_, opts)
+      local cmp = require("cmp")
 
-{
-  "hrsh7th/nvim-cmp",
-  config = function(_, opts)
-    local cmp = require("cmp")
+      -- Ensure mapping table exists
+      opts.mapping = opts.mapping or {}
 
-    -- Ensure mapping table exists
-    opts.mapping = opts.mapping or {}
+      -- Remove Enter confirmation entirely
+      opts.mapping["<CR>"] = nil
 
-    -- Remove Enter confirmation entirely
-    opts.mapping["<CR>"] = nil
+      -- Accept completion ONLY with Ctrl+y
+      opts.mapping["<C-y>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      })
 
-    -- Accept completion ONLY with Ctrl+y
-    opts.mapping["<C-y>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = false,
-    })
-
-    -- Apply FULL config (sources, snippets, everything)
-    cmp.setup(opts)
-  end,
-},
+      -- Apply FULL config (sources, snippets, everything)
+      cmp.setup(opts)
+    end,
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "odin"
+  		},
+
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+
+      indent = { enable = true },
+  	},
+  },
+    {
+   "stevearc/conform.nvim",
+   opts = {
+      notify_on_error = false,
+      -- Odinfmt gets its configuration from odinfmt.json. It defaults
+      -- writing to stdout but needs to be told to read from stdin.
+      formatters = {
+         odinfmt = {
+            -- Change where to find the command if it isn't in your path.
+            command = "odinfmt",
+            args = { "-stdin" },
+            stdin = true,
+         },
+      },
+      -- and instruct conform to use odinfmt.
+      formatters_by_ft = {
+         odin = { "odinfmt" },
+      },
+   },
+}
 }
