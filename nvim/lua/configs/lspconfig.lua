8ada13aa46464307796fcf8085ab818a40dcb68b
Christian Stout <christian.allen.stout@gmail.com>
2026-03-06 19:44:54 -0500

    added go lsp to nvim

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ols", "lua-language-server" }
+local servers = { "html", "cssls", "ols", "lua-language-server", "eslint-lsp", "gopls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
